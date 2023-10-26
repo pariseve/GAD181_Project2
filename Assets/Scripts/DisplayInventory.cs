4827741a0971673dfbc77bb151d764f6f0350fbf
pariseve <136556166+pariseve@users.noreply.github.com>
2023-10-26 22:32:46 +1100

    updated inventory system
    displays different ui within the iventory prefab depending on what item object is interacted with

--- Assets/Scripts/DisplayInventory.cs
+++ Assets/Scripts/DisplayInventory.cs
@@ -2,67 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class DisplayInventory : MonoBehaviour
 {
     public GameObject inventoryPrefab;
     public InventoryObject inventory;
+    public int X_START;
+    public int Y_START;
+    public int X_SPACE_BETWEEN_ITEM;
+    public int NUMBER_OF_COLUM;
+    public int Y_SPACE_BETWEEN_ITEMS;
 
-    public int xStart;
-    public int yStart;
-    public int xSpaceBetweenItem;
-    public int numberOfColumn;
-    public int ySpaceBetweenItem;
     Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
-    // Start is called before the first frame update
+
     void Start()
     {
         CreateDisplay();
     }
-    //private void Update()
-    //{
-    //    UpdateDisplay();
-    //}
-    //update is called once per frame
+
+    void Update()
+    {
+        UpdateDisplay();
+    }
+
     public void UpdateDisplay()
     {
-        Debug.Log("Update Display is called");
         for (int i = 0; i < inventory.Container.Items.Count; i++)
         {
             InventorySlot slot = inventory.Container.Items[i];
-            // Check if the slot is already displayed in the inventory
+
             if (itemsDisplayed.ContainsKey(slot))
             {
-                // Use null conditional operator to access the TextMeshProUGUI component
-                TextMeshProUGUI textComponent = itemsDisplayed[slot]?.GetComponentInChildren<TextMeshProUGUI>();
-
-                if (textComponent != null)
-                {
-                    textComponent.text = slot.amount.ToString("n0");
-                }
+                itemsDisplayed[slot].GetComponentInChildren<TextMeshProUGUI>().text = slot.amount.ToString("n0");
             }
             else
             {
                 var obj = Instantiate(inventoryPrefab, Vector3.zero, Quaternion.identity, transform);
-                Debug.Log("Instantiated prefab: " + obj.name);
-
-                // Use null conditional operator to access the Image component
-                //Image imageComponent = obj?.transform.GetChild(0)?.GetComponentInChildren<Image>();
-                //if (imageComponent != null)
-                //{
-                //    imageComponent.sprite = inventory.database.GetItem[slot.item.Id].uiDisplay;
-                //}
-
+                obj.transform.GetChild(0).GetComponentInChildren<Image>().sprite = inventory.database.GetItem[slot.item.Id].uiDisplay;
                 obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-
-                // Use null conditional operator to access the TextMeshProUGUI component
-                TextMeshProUGUI textComponent = obj?.GetComponentInChildren<TextMeshProUGUI>();
-                if (textComponent != null)
-                {
-                    textComponent.text = slot.amount.ToString("n0");
-                }
-
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = slot.amount.ToString("n0");
                 itemsDisplayed.Add(slot, obj);
             }
         }
@@ -76,14 +57,23 @@ public class DisplayInventory : MonoBehaviour
 
             var obj = Instantiate(inventoryPrefab, Vector3.zero, Quaternion.identity, transform);
             obj.transform.GetChild(0).GetComponentInChildren<Image>().sprite = inventory.database.GetItem[slot.item.Id].uiDisplay;
-            Debug.Log("Instantiated prefab: " + obj.name);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = slot.amount.ToString("n0");
+
             itemsDisplayed.Add(slot, obj);
         }
     }
     public Vector3 GetPosition(int i)
     {
-        return new Vector3(xStart + (xSpaceBetweenItem * (i % numberOfColumn)), yStart + (-ySpaceBetweenItem * (i / numberOfColumn)), 0f);
+        if (NUMBER_OF_COLUM != 0)
+        {
+            return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUM)), Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i / NUMBER_OF_COLUM)), 0f);
+        }
+        else
+        {
+            // Handle the case where NUMBER_OF_COLUM is zero, possibly by returning a default position.
+            // For example, if you want to center an item in this case:
+            return new Vector3(0, 0, 0);
+        }
     }
 }
