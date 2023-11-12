848b54b97743a36aea508ee05e1bdf92f44c83de
pariseve <136556166+pariseve@users.noreply.github.com>
2023-11-12 20:22:18 +1100

    added animations to player character
    altered playercontroller script

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -8,43 +8,63 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public float groundDistance = 0.1f;
-    public float smoothingFactor = 10f; // Adjust this value for smoother movement
+    public float smoothingFactor = 10f;
     public LayerMask groundLayer;
     public Rigidbody rb;
     public SpriteRenderer sr;
+    public Animator animator;
 
     public DisplayInventory displayInventory;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.interpolation = RigidbodyInterpolation.Interpolate; // Use interpolation for smoother movement
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        animator = GetComponent<Animator>();
+
+        // Add MeshCollider if not added already
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
+        if (meshCollider == null)
+        {
+            meshCollider = gameObject.AddComponent<MeshCollider>();
+            // Configure MeshCollider as needed
+        }
     }
 
     void Update()
     {
         RaycastHit hit;
-        Vector3 castPos = transform.position + Vector3.up; // Slightly above the character
+        Vector3 castPos = transform.position + Vector3.up;
         if (Physics.Raycast(castPos, -Vector3.up, out hit, Mathf.Infinity, groundLayer))
         {
             float targetY = hit.point.y + groundDistance;
             Vector3 targetPosition = new Vector3(transform.position.x, targetY, transform.position.z);
-
-            // Apply smoothing to character's position
             transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothingFactor);
         }
 
         float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        Vector3 moveDirection = new Vector3(x, 0, y);
+        float z = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(x, 0, z);
 
-        // Apply movement directly to the Rigidbody for smoother physics
         rb.velocity = moveDirection * speed;
 
         // Handle character orientation (flipping sprite)
-        if (x != 0)
+        if (x != 0 || z != 0) // Check if moving in either x or z direction
         {
-            sr.flipX = (x > 0);
+            // Set parameters for Animator
+            bool isRunning = moveDirection.magnitude > 0; // Check if the player is moving
+            animator.SetBool("IsRunning", isRunning);
+
+            // Flip the character in the opposite direction
+            Vector3 localScale = transform.localScale;
+            localScale.x = Mathf.Abs(localScale.x) * -Mathf.Sign(x); 
+            localScale.z = Mathf.Abs(localScale.z) * -Mathf.Sign(z); 
+            transform.localScale = localScale;
+        }
+        else
+        {
+            // If not moving, set IsRunning to false
+            animator.SetBool("IsRunning", false);
         }
 
         //if (Input.GetMouseButtonDown(1)) // Right-click
